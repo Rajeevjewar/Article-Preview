129c1314d6341c4797fa113efa0d91feabb4724a
RajeevKumar <rajeevjewar2@gmail.com>
2023-03-10 01:47:15 +0530

    Article Preview Component updated now

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const shareBtn = document.querySelector(".share__link");
 const shareMedia = document.querySelector(".share__media");
-
+const card = document.querySelector(".card");
 shareBtn.addEventListener("click", () => {
   shareMedia.classList.toggle("hidden");
 });
